316bd3c1d99517b77bc7f708bcfe20637f61a359
dontscrewme <57172435+dontscrewme@users.noreply.github.com>
2025-06-19 14:51:37 +0800

    Update dictionary.c

--- dictionary.c
+++ dictionary.c
@@ -130,7 +130,7 @@ const char *dictionary_get(const struct dictionary *d, const char *key,
 }
 
 int dictionary_set(struct dictionary *d, const char *key, const char *val) {
-	if (!d || !key) {
+	if (!d || !key || !val) {
 		error_callback("%s: invalid input\n", __func__);
 		return -1;
 	}
